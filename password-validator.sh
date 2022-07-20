1c2e4881d648321fdf593d7a7f884b5e73b184c1
root <root@DeepaliGhadge.localdomain>
2022-07-20 15:55:05 +0530

    Added pasword-validator bash script

---
+++ password-validator.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#User password input field
+echo "Enter password: "
+read password
+
+#Global Variable to store password
+len="${#password}"
+
+#Variables to store color codes
+green='\033[0;32m'
+red='\033[0;31m'
+clear='\033[0m'
+
+#To check password length is greater than or equal to 10
+if test $len -ge 10 ; then
+
+	#To check password contains numbers
+	 echo "$password" | grep -q [0-9]
+		if test $? -eq 0; then
+
+			#To check password contains capital alphabets
+			echo "$password" | grep -q [A-Z]
+				if test $? -eq -0; then
+
+					#To check password contains small alphabets
+					echo "$password" | grep -q [a-z]
+						if test $? -eq 0; then					
+							echo -e "${green}password length valid + contains number + captial + small alphabets ${clear}"
+							echo "Exit code is : " $? 
+							exit 0
+						else
+							echo -e  "${red}password lenght valid + contains number + capital BUT NO small alphabets ${clear}" >&2
+							exit 1
+							echo "Exit Code is : " $?				
+						fi
+				else
+					echo -e "${red} password length valid + contains number + Small alphahets BUT no capital alphabets ${clear}"
+					#exit 1
+					#echo "Exit code is :" $?
+				fi
+		else
+			echo -e "${red}password length valid + BUT no numbers AND small AND capital alpbahets ${clear} "
+			#exit 1
+			#echo "Exit Code is : " $? 
+				
+		fi
+else
+		echo -e "${red}password length is invalid, should be at least 10 ${clear}"
+		#exit 1
+		#echo "Exit code is : " $?
+fi
